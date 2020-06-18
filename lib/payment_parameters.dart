e30c38739e1df1c679364a042982fdbafa59d3fc
gygyms <gygygyms@gmail.com>
2020-06-18 13:39:21 +0300

    добавлен public id в dart

--- lib/payment_parameters.dart
+++ lib/payment_parameters.dart
@@ -3,15 +3,17 @@ class PaymentParameters{
   String cardNumber;
   String cardDate; //формат MMYY
   String cardCVV;
+  String publicId;
 
 
-  PaymentParameters(this.cardNumber, this.cardDate, this.cardCVV);
+  PaymentParameters(this.cardNumber, this.cardDate, this.cardCVV,this.publicId);
 
   Map<String,String> toMap(){
     var map = Map<String,String>();
     map['card_number'] = cardNumber;
     map['card_date'] = cardDate;
     map['card_cvv'] = cardCVV;
+    map['public_id'] = publicId;
     return map;
   }
 }
