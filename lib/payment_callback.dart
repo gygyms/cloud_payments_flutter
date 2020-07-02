11ef3a27f10f22160ed41d4693da8a44ad8f7d76
gygyms <gygygyms@gmail.com>
2020-07-02 13:05:34 +0300

    Добавлен коллбэк для googlePay и ошибок

--- lib/payment_callback.dart
+++ lib/payment_callback.dart
@@ -3,6 +3,10 @@ import 'package:flutter/cupertino.dart';
 typedef void CryptogramSuccessCallbackFunction(String cryptogram,BuildContext context);
 typedef void CryptogramErrorCallbackFunction(String error, BuildContext context);
 typedef void RecurringCallbackFunction(String token, BuildContext context);
+typedef void GooglePaySuccessCallbackFunction(String cryptogram,BuildContext context);
+typedef void ErrorCallbackFunction(String error,BuildContext context);
+
+
 
 
 class PaymentCallback{
@@ -10,8 +14,12 @@ class PaymentCallback{
   CryptogramErrorCallbackFunction onCardPaymentError;
 
   RecurringCallbackFunction onRecurrentPaymentCallback;
+  GooglePaySuccessCallbackFunction onGooglePaySuccessCallback;
+  ErrorCallbackFunction onError;
 
-  PaymentCallback(this.onCardPaymentCallback,
-      this.onCardPaymentError,this.onRecurrentPaymentCallback); //(String error, BuildContext context);
+  PaymentCallback(this.onCardPaymentCallback, this.onCardPaymentError,
+      this.onRecurrentPaymentCallback, this.onGooglePaySuccessCallback,
+      this.onError);
+
 
 }
